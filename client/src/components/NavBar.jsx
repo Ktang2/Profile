048312c8aaf7ee08503aa2b0e321eef81072ae04
Kalvin Tang <kaltang@kalvins-mbp.mynetworksettings.com>
2023-11-02 21:58:34 -0400

    added github and linkedin links

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import {Link, NavLink} from 'react-router-dom';
-import {FaBars, FaTimes} from "react-icons/fa";
+import {FaGithub, FaLinkedin} from 'react-icons/fa';
 import './NavBar.css'
 
 const NavBar = () =>{
@@ -16,9 +16,13 @@ const NavBar = () =>{
             </div>
             <ul className={menuOpen ? "open" : ""}>
                 <li><NavLink to="/about">About</NavLink></li>
+                <li><NavLink to="/skills">Skills</NavLink></li>
                 <li><NavLink to="/projects">Projects</NavLink></li>
-                <li><NavLink to="/services">Services</NavLink></li>
                 <li><button className="contact-btn">Contact</button></li>
+                <div className="link-icons">
+                    <Link className="link-icon"to="https://github.com/Ktang2"><FaGithub style={{ fontSize: '2rem' }}/></Link>
+                    <Link className="link-icon" to="https://www.linkedin.com/in/kalvin-tang-b0b80575/"><FaLinkedin  style={{ fontSize: '2rem' }}/></Link>
+                </div>
             </ul>
         </nav>
     )
